a9c75113afc512f3bb8e226bf1e27406ce9fbc1c
Anthony Ruhier <anthony.ruhier@gmail.com>
2016-06-01 15:15:17 +0200

    First batch of room

--- scripts/room.js
+++ scripts/room.js
@@ -1,8 +1,19 @@
 "using strict";
 
+class Opening {
+  constructor(orientation, posx=0, posy=0) {
+    this.posx = posx;
+    this.posy = posy;
+    this.orientation = orientation;
+  }
+}
+
 class Room {
-  constructor(args) {
-    // code
+  constructor(posx=0, posy=0, height=0, width=0) {
+    this.posx = posx;
+    this.posy = posy;
+    this.height = height;
+    this.width = width;
   }
 }
 
