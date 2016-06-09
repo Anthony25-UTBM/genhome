5600334e46969a0f45daa2538d1c5bc96252fbe1
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-06-09 15:11:39 +0200

    Fixed integration test.

--- spec/ui.spec.js
+++ spec/ui.spec.js
@@ -1,36 +1,46 @@
 describe('Room management', () => {
-  const createRoom = function(name) {
-    gen_home.toolbar.room_list.add_room(room.name);
-  };
-
-  beforeEach(() => {
-    browser.url('http://localhost:3000');
-  });
-
 
   it('can create a room', () => {
-    browser.click('.js-room-list .toolbar__list-item:last-child');
 
+    // Open browser in local fileserver
+    browser.url('http://localhost:3000');
+
+    // Click on the 'Nouvelle pièce' button
+    browser.click('label=Nouvelle pièce ...');
+
+    // Assert UI room list length to be 1 (the 'Nouvelle pièce' button)
+    let roomListItems = browser.elements('.js-room-list .toolbar__list-item');
+    expect(roomListItems.value.length).toBe(1);
+
+    // Assert modal container visible
     let modalContainerClasses = browser.getAttribute('.modal__container', 'class');
     expect(modalContainerClasses).toContain('modal__container--visible');
 
+    // Assert modal visible
     let roomDialogClasses = browser.getAttribute('.modal.js-room-modal', 'class');
     expect(roomDialogClasses).toContain('modal--visible');
 
-    // browser.waitUntil(() => {});
+    // Fill the form
+    browser.click('input#room-modal__name');
+    browser.keys('Chambre');
+    browser.keys('\uE004'); // Press TAB key
+    browser.keys('5');
+    browser.keys('\uE004'); // Press TAB key
+    browser.keys('4');
 
-    // // Fill the form
-    // browser.click('input#room-modal__name');
-    // browser.keys('Chambre');
-    // browser.click('input#room-modal__length');
-    // browser.keys('5');
-    // browser.click('input#room-modal__width');
-    // browser.keys('4');
+    // Click 'Create' button
+    browser.click('.js-room-modal .form__action--primary');
 
-    // Submit the form through the keyboard
-    browser.keys('\uE007');
+    // Assert modal container not visible
+    modalContainerClasses = browser.getAttribute('.modal__container', 'class');
+    expect(modalContainerClasses).not.toContain('modal__container--visible');
 
-    let roomListItems = browser.elementIdElements(browser.element('.js-room-list').value, '.toolbar__list-item');
-    console.log(roomListItems);
+    // Assert modal not visible
+    roomDialogClasses = browser.getAttribute('.modal.js-room-modal', 'class');
+    expect(roomDialogClasses).not.toContain('modal--visible');
+
+    // Assert UI room list length to be 2 (the room and the 'Nouvelle pièce' button)
+    roomListItems = browser.elements('.js-room-list .toolbar__list-item');
+    expect(roomListItems.value.length).toBe(2);
   });
 });
